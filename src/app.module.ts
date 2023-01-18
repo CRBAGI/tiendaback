e3a9eaaf7110cf688b210a997654f5412378fd6c
CRBAGI <brayanal0623@gmail.com>
2023-01-17 19:20:54 -0600

    elmer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
+import { ProductoModule } from './Modules/producto/producto.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ProductoModule,],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
