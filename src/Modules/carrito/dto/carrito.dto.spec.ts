13929eb335604ca18e5233d0b5dddeef231fb056
AthonLT08 <antoniomejia.8@hotmail.com>
2023-01-18 17:25:19 -0600

    dto

---
+++ src/Modules/carrito/dto/carrito.dto.spec.ts
@@ -0,0 +1,7 @@
+import { CarritoDto } from './carrito.dto';
+
+describe('CarritoDto', () => {
+  it('should be defined', () => {
+    expect(new CarritoDto()).toBeDefined();
+  });
+});
---
+++ src/Modules/landing/dto/landing.dto.spec.ts
@@ -0,0 +1,7 @@
+import { LandingDto } from './landing.dto';
+
+describe('LandingDto', () => {
+  it('should be defined', () => {
+    expect(new LandingDto()).toBeDefined();
+  });
+});
