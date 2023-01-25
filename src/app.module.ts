0fdb85518f616514802881b810a8e5c7dfcfba74
CRBAGI <brayanal0623@gmail.com>
2023-01-24 18:56:28 -0600

    24 de enero

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LandingModule } from './Modules/landing/landing.module';
-import { LandingController } from './Modules/landing/controller/landing.controller';
-import { CarritoController } from './Modules/carrito/controller/carrito.controller';
-import { LoginController } from './Modules/Login/controller/login.controller';
-import { ProductoController } from './Modules/producto/controller/producto.controller';
 import { CarritoModule } from './Modules/carrito/carrito.module';
-import { LoginModule } from './Modules/Login/login.module';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { usuarioModule } from './Modules/Login/login.module';
+
 
 @Module({
   imports: [
-    ProductoModule, LandingModule, CarritoModule, LoginModule,],
-  controllers: [AppController, LandingController, CarritoController, LoginController,ProductoController],
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username:'root',
+      password: '',
+      database: 'tienda',
+      autoLoadEntities: true,
+      
+    }),
+    ProductoModule, 
+    LandingModule, 
+    CarritoModule, 
+    usuarioModule,
+    ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
