b15509f3b19fdef0ff866e4c5c664d3488026a2a
AthonLT08 <antoniomejia.8@hotmail.com>
2023-02-22 19:01:17 -0600

    listo

--- src/Modules/Login/repository/usuario.repository.ts
+++ src/Modules/Login/repository/usuario.repository.ts
@@ -5,11 +5,11 @@ import { RegistroDto } from "../dto/registro.dto";
 
 @EntityRepository(usuario)
 export class usuarioRepository extends Repository<usuario> {
-       async createUsuario(RegistroUsuarioDto: RegistroDto): Promise<void>{
-        const{nombre, email, password, rol, sexo, fechaDeNacimiento, fotoDePerfil}=RegistroUsuarioDto;
-        const usuario= this.create({nombre, email, password, rol, sexo, fechaDeNacimiento, fotoDePerfil});
-await this.save(usuario);
-       }
+        async createUsuario(RegistroDto: RegistroDto): Promise<void> {
+                const { nombre, email, password, rol, sexo, fechaDeNacimiento, fotoDePerfil } = RegistroDto;
+                const usuario = this.create({ nombre, email, password, rol, sexo, fechaDeNacimiento, fotoDePerfil });
+                await this.save(usuario);
+        }
 }
 
 
